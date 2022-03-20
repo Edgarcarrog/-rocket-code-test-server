61f2ffe9e83bfce6da86d5fad0da1e43e2081335
Edgarcarrog <edgc21@hotmail.com>
2022-03-20 17:48:04 -0600

    cors headers

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,15 @@ app.set("port", process.env.PORT || 4000);
 
 //middlewares
 app.use(cors());
-app.options('*', cors())
+app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 //Routes
