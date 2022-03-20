ab5110d93ec18c14595a47b784b65b08812ec09d
Edgarcarrog <edgc21@hotmail.com>
2022-03-20 17:53:35 -0600

    cors domain

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ app.set("port", process.env.PORT || 4000);
 app.use(cors());
 app.options("*", cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
