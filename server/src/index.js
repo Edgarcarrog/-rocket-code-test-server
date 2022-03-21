2c22179b99597f718625b840761483e58f441a25
Edgarcarrog <edgc21@hotmail.com>
2022-03-20 18:41:37 -0600

    cors headers domain all

--- server/src/index.js
+++ server/src/index.js
@@ -9,8 +9,9 @@ app.set("port", process.env.PORT || 4000);
 
 //middlewares
 app.use(cors());
+//app.options("*", cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
